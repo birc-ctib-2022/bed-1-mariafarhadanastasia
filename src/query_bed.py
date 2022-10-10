bb6ecc8cf19883e75c535190b2a69840216d857a
Farhad Zamani <zmnfrhd@gmail.com>
2022-10-10 21:07:01 +0200

    add doctest and function description

--- src/query_bed.py
+++ src/query_bed.py
@@ -9,7 +9,6 @@ from bed import (
 )
 from query import Table
 
-
 def get_bed_table(infile: TextIO) -> Table:
     """
     Get and insert the bed file into a 'Table'.
@@ -18,17 +17,16 @@ def get_bed_table(infile: TextIO) -> Table:
     bed_table = Table()
     for line in infile:
         bed_table.add_line(parse_line(line))
-
     return bed_table
-
 
 def is_overlapping(query_lower: int, query_upper: int, feature_start: int, feature_end: int) -> bool:
     """
     This function wants to check whether the feature in our BED file is within the query range or not.
-    > query_lower:
-    > query_upper:
-    > feature_start:
-    > feature_end:
+    >> query_lower - [feature_start:feature_end] - query_upper
+    > query_lower: Start index of query.
+    > query_upper: Last index of query.
+    > feature_start: Start index of a feature
+    > feature_end: Last index of a feature
 
     >>> is_overlapping(0, 100, 2, 3)
     True
@@ -40,30 +38,41 @@ def is_overlapping(query_lower: int, query_upper: int, feature_start: int, featu
     else:
         return False
 
-
 def split_query_line(line: str) -> Tuple[str, int, int]:
     """
-    >>> split_query_line("chrom5  841     842")
-    ('chrom5', 841, 842)
+    Split a line that have chromosome, query start index, and query
+    last index information.
+    
+    >>> split_query_line("chrom5  841     900")
+    ('chrom5', 841, 900)
+
+    >>> split_query_line("xx  0     1")
+    ('xx', 0, 1)
     """
     chrom, start, end = line.split()
     return chrom, int(start), int(end)
 
+def print_overlap(bedlines: Table, query_lower: int, query_upper: int, output: TextIO) -> None:
+    """
+    Print every BedLine to outfile or stdout that overlaps with given range of query.
+    BedLines only contains all BedLine with certain "chrom"
+    """
+    for i in range(0, len(bedlines)):
+        feature_start = bedlines[i].chrom_start
+        feature_end = bedlines[i].chrom_end
+        if is_overlapping(query_lower, query_upper, feature_start, feature_end):
+            print_line(bedlines[i], output)
+    return None
 
 def process_query(fbed: TextIO, fquery: TextIO, fout:TextIO) -> None:
-    """
-    """
+    """Function to process all files: bed, query, and outfile (if any)."""
     bed_table = get_bed_table(fbed)
 
     for line in fquery:
         chrom, query_start, query_end = split_query_line(line)
-        bedline = bed_table.get_chrom(chrom)
-
-        for i in range(0, len(bedline)):
-                if is_overlapping(query_start, query_end, bedline[i].chrom_start, bedline[i].chrom_end):
-                    print_line(bedline[i], fout)
+        bedlines = bed_table.get_chrom(chrom) # bedlines contains all BedLine with certain "chrom"
+        print_overlap(bedlines, query_start, query_end, fout)
     return None
-
 
 def main() -> None:
     """Run the program."""
